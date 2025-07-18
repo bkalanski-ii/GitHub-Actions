f7473184c3d360f6483305caa2a7197ff67cb319
Bart Kalanski <bart.kalanski@ii.co.uk>
2025-07-18 11:50:29 +0100

    Create react app

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig, type UserConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    // 👋 add the line below to add jsdom to vite
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/tests/setup.ts',
+  }
+} as UserConfig)
